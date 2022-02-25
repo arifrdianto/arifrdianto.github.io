3d080a2d07f01b8a6da53b608a78b207562ebf74
Arif Rudianto <rudianto.arif@nutech-integrasi.com>
2022-02-25 15:36:14 +0700

    chore: add gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 /** @type {import('gatsby').GatsbyConfig} */
 module.exports = {
+  pathPrefix: "/arifrdianto.github.io",
   siteMetadata: {
-      siteUrl: `https://www.yourdomain.tld`,
+      siteUrl: `https://arifrdianto.github.io`,
   },
   plugins: [
     'gatsby-plugin-postcss'
